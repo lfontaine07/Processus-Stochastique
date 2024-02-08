f7d9000dc5511e5dc0d316e035b36524f66ee772
Loïc FONTAINE <loic.fontaine@esiroi.re>
2024-02-08 10:08:23 +0400

    Les matrices après n-jour en determinant les etat initial

--- Processus_Stochastique_TD1_Exo1.py
+++ Processus_Stochastique_TD1_Exo1.py
@@ -4,11 +4,32 @@ import numpy as np
 #matrice de transition
 matrice_transistion= np.array([[0.7, 0.2, 0.1], [0.5, 0.3, 0.2], [0.4, 0.3, 0.3]])
 #etat initial ensoleilée 
-etat_intitial= np.array([[1], [0], [0]])
+etat_intitial_soleil= np.array([[1], [0], [0]])
 #matrice de transition pour avoir l'état dans deux jours
 matrice_carré = matrice_transistion.dot(matrice_transistion)
 
 #etat dans deux jours
-etat_apr_2j= np.transpose(etat_intitial).dot(matrice_carré)
+etat_apr_2j= np.transpose(etat_intitial_soleil).dot(matrice_carré)
 
-print(etat_apr_2j)
+
+def simulation (n_jours) : 
+
+    #les état initiaux possible
+    etat_intitial_soleil= np.array([[1], [0], [0]])
+    etat_intitial_nuageux= np.array([[0], [1], [0]])
+    etat_intitial_pluvieux= np.array([[0], [0], [1]])
+
+    #matrice de transition 
+    matrice_transistion= np.array([[0.7, 0.2, 0.1], [0.5, 0.3, 0.2], [0.4, 0.3, 0.3]])
+
+    jours = list(range(n_jours))
+
+    for i in jours :
+
+        #etat aprres n jours
+        etat_apr_n_jours = np.transpose(etat_intitial_nuageux).dot(np.linalg.matrix_power(matrice_transistion, i))
+        print("Matrice  : ", etat_apr_n_jours)
+
+simulation(20)
+
+
