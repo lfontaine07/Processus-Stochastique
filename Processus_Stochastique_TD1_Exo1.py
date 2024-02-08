9fb7300841ca90fcb04e9b18dfca465ad58f832f
Loïc FONTAINE <loic.fontaine@esiroi.re>
2024-02-08 10:46:52 +0400

    La le code donne les matrice des probalité des 3 état à un jours n

--- Processus_Stochastique_TD1_Exo1.py
+++ Processus_Stochastique_TD1_Exo1.py
@@ -19,16 +19,37 @@ def simulation (n_jours) :
     etat_intitial_nuageux= np.array([[0], [1], [0]])
     etat_intitial_pluvieux= np.array([[0], [0], [1]])
 
-    #matrice de transition 
     matrice_transistion= np.array([[0.7, 0.2, 0.1], [0.5, 0.3, 0.2], [0.4, 0.3, 0.3]])
 
     jours = list(range(n_jours))
 
-    for i in jours :
+    etat_intitial = etat_intitial_soleil
 
-        #etat aprres n jours
-        etat_apr_n_jours = np.transpose(etat_intitial_nuageux).dot(np.linalg.matrix_power(matrice_transistion, i))
-        print("Matrice  : ", etat_apr_n_jours)
+    if np.all(etat_intitial == etat_intitial_soleil) : 
+
+        for i in jours :
+
+            #etat aprres n jours
+            etat_apr_n_jours = np.transpose(etat_intitial).dot(np.linalg.matrix_power(matrice_transistion, i))
+            print("Matrice  : ", etat_apr_n_jours)
+
+    if np.all(etat_intitial == etat_intitial_nuageux) :
+
+        for i in jours :
+
+            #etat aprres n jours
+            etat_apr_n_jours = np.transpose(etat_intitial).dot(np.linalg.matrix_power(matrice_transistion, i))
+            print("Matrice  : ", etat_apr_n_jours)
+
+    if np.all(etat_intitial == etat_intitial_pluvieux):  
+
+        for i in jours :
+
+            #etat aprres n jours
+            etat_apr_n_jours = np.transpose(etat_intitial).dot(np.linalg.matrix_power(matrice_transistion, i))
+            print("Matrice  : ", etat_apr_n_jours)
+
+
 
 simulation(20)
 
